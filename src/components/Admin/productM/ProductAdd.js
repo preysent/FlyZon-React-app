3007927adb2e0da1cb70545d9983a917b4f045ef
preysent <preysent@gmail.com>
2023-07-28 22:15:12 +0530

    compleated the form to add new product

--- src/components/Admin/productM/ProductAdd.js
+++ src/components/Admin/productM/ProductAdd.js
@@ -1,66 +1,108 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addNewProduct } from '../../../redux/slices/sellerSlice'
 
 const ProductAdd = () => {
 
-      //initial state of form elemnt
-  const [credentials, setCredentials] = useState({
-    productTitle:"",
-    description:[],
-    price:"",
-    brand:"",
-    category:"",
-    stock:"",
-    images:[]
+    const dispatch = useDispatch()
+    //initial state of form elemnt
+    const [credentials, setCredentials] = useState({
+        productTitle: "",
+        des: "",
+        price: "",
+        brand: "",
+        category: "",
+        stock: "",
+        img: ""
 
-  })
+    })
 
 
-  const { productTitle,  description,  price,  brand,  category,  stock,  images }=credentials
+    const { productTitle, des, price, brand, category, stock, img } = credentials
+
+
+    const onchange = (event) => {
+        setCredentials({ ...credentials, [event.target.name]: event.target.value })
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        // converting string to array of values 
+        let description = des.split("\n")
+        description = description.filter((vl) => { return vl.length > 0 })
+
+
+        // Regular expression to match links
+        const linkPattern = /https?:\/\/\S+/gi;
+
+        let images = img.split("\n")
+        images = images.filter((link) => { // Only return Link
+            return linkPattern.test(link)
+        })
+
+        console.log("dispatch called")
+        // creating acrion for adding product 
+        const res = await dispatch(addNewProduct({ productTitle, price, brand, category, stock, description, images }))
+
+        if (res.payload.status) {
+            alert("product created sussefully")
+            setCredentials({
+                productTitle: "", des: "", price: "", brand: "", category: "", stock: "", img: ""
+            })
+        } else {
+            alert("!..Failed to create product")
+        }
+
+    }
     return (
         <div>
             {/* Add New Product Form  */}
             <section>
+
                 <h2 className="text-2xl font-bold mb-4">Add New Product</h2>
-                <form id="addProductForm" className="bg-white rounded shadow p-6">
+                <form onSubmit={handleSubmit} id="addProductForm" className="bg-white rounded shadow p-6">
 
                     {/* Form fields for adding a new product  */}
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-full px-3">
                             <label htmlFor="name" className="block text-gray-700 font-semibold">Product Name</label>
-                            <input type="text" id="name" name="productTitle" value={productTitle} required className="w-full p-2 border rounded" />
+                            <input onChange={onchange} type="text" id="name" name="productTitle" value={productTitle} required className="w-full p-2 border rounded" />
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-full px-3">
-                            <label htmlFor="description" className="block text-gray-700 font-semibold">Product Description</label>
-                            <textarea id="description" name="description" required className="w-full p-2 border rounded"></textarea>
+                            <label htmlFor="des" className="block text-gray-700 font-semibold">Product Description</label>
+                            <textarea onChange={onchange} id="des" name="des" value={des} required className="w-full p-2 border rounded" placeholder='New point in new line'></textarea>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-1/2 px-3">
                             <label htmlFor="price" className="block text-gray-700 font-semibold">Price</label>
-                            <input type="number" id="price" name="price" value={price} required className="w-full p-2 border rounded" />
+                            <input onChange={onchange} type="number" id="price" name="price" value={price} required className="w-full p-2 border rounded" min="0" />
                         </div>
                         <div className="w-1/2 px-3">
                             <label htmlFor="brand" className="block text-gray-700 font-semibold">Brand</label>
-                            <input type="text" id="brand" name="brand" required className="w-full p-2 border rounded" />
+                            <input onChange={onchange} type="text" id="brand" name="brand" value={brand} required className="w-full p-2 border rounded" />
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-1/2 px-3">
                             <label htmlFor="category" className="block text-gray-700 font-semibold">Category</label>
-                            <input type="text" id="category" name="category" required className="w-full p-2 border rounded" />
+                            <input onChange={onchange} type="text" id="category" value={category} name="category" required className="w-full p-2 border rounded" />
                         </div>
                         <div className="w-1/2 px-3">
                             <label htmlFor="stock" className="block text-gray-700 font-semibold">Stock</label>
-                            <input type="number" id="stock" name="stock" required className="w-full p-2 border rounded" />
+                            <input onChange={onchange} type="number" id="stock" value={stock} name="stock" required className="w-full p-2 border rounded" min="0" />
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-full px-3">
-                            <label htmlFor="images" className="block text-gray-700 font-semibold">Images</label>
-                            <input type="text" id="images" name="images" required className="w-full p-2 border rounded" />
+                            <label htmlFor="img1" className="block text-gray-700 font-semibold">Images</label>
+                            <textarea onChange={onchange} type="text" id="img" name="img" value={img} required className="w-full p-2 border rounded" placeholder='Only for image links | New link in new line' ></textarea>
                         </div>
+
                     </div>
                     <div className="flex justify-end">
                         <button type="submit"
--- src/redux/slices/sellerSlice.js
+++ src/redux/slices/sellerSlice.js
@@ -3,44 +3,60 @@ const host = process.env.REACT_APP_API_URL
 
 
 // fucntin to getting product list of the seller
-export const getProductList = createAsyncThunk('getProductList',async()=>{
-    const responce = await fetch(`${host}/api/seller/products`,{
-        headers:{
+export const getProductList = createAsyncThunk('getProductList', async () => {
+    const responce = await fetch(`${host}/api/seller/products`, {
+        headers: {
             "Content-type": "application/json; charset=UTF-8",
             "authToken": localStorage.getItem('authToken')
         }
     })
     return await responce.json()
-   
+
 })
 
-const  sellerSlice = createSlice({
-    name:'seller',
-    initialState:{
-        productList:[],
-        seller:false,
-        isLoading:false,
-        isError:false
+
+// function to add new product 
+export const addNewProduct = createAsyncThunk('addNewProduct', async (details) => {
+    const requestBody = JSON.stringify(details)
+    console.log("comming in dispatch")
+    const responce = await fetch(`${host}/api/product`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "authToken": localStorage.getItem('authToken')
+        },
+        body: requestBody
+    })
+    return await responce.json()   
+})
+
+const sellerSlice = createSlice({
+    name: 'seller',
+    initialState: {
+        productList: [],
+        seller: false,
+        isLoading: false,
+        isError: false
     },
-    extraReducers:(builder)=>{
-        
+    extraReducers: (builder) => {
+
         // case for productList 
-        builder.addCase(getProductList.pending,(state)=>{
+        builder.addCase(getProductList.pending, (state) => {
             state.isLoading = true
             state.isError = false
         })
-        builder.addCase(getProductList.fulfilled,(state,action)=>{
+        builder.addCase(getProductList.fulfilled, (state, action) => {
             state.isLoading = false
-            if(action.payload.status){
+            if (action.payload.status) {
                 state.isError = false
                 state.productList = action.payload.productList
             }
             else state.isError = true
         })
-        builder.addCase(getProductList.rejected,(state)=>{
+        builder.addCase(getProductList.rejected, (state) => {
             state.isError = true
             state.isLoading = false
-            
+
         })
 
     }
