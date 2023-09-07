1bbcbf3a98f934c6347f76529c35bd11de0765b1
Praveen Parihar <117361697+preysent@users.noreply.github.com>
2023-09-07 17:21:52 +0530

    Update Main.js

--- src/components/body/Main.js
+++ src/components/body/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Home from './Home'
 import ProductPage from './ProductPage';
 import { Switch, Route } from "react-router-dom";
